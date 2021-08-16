6e572d787b91f9b025186e3d4d0c74825dfd9500
welcomeworld <guduziyousiwang@gmail.com>
2021-08-16 14:55:53 +0800

    配置ffmpeg

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include <string>
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_github_welcomeworld_bipplayer_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = avcodec_configuration();
+    return env->NewStringUTF(hello.c_str());
+}
